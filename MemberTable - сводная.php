80fa964ea66795f9948c9afe8132f14f64e16eab
VitaliiSestrenskyi <sestrinskiy@mail.ru>
2016-12-26 13:15:57 +0200

    MemberTable - сводная.php
    MemberTable - сводная пример сводной таблицы

---
+++ MemberTable - сводная.php
@@ -0,0 +1,78 @@
+<?php
+namespace Bitrix\Tasks\Internals\Task;
+
+use Bitrix\Main,
+	Bitrix\Main\Localization\Loc;
+//Loc::loadMessages(__FILE__);
+
+class MemberTable extends Main\Entity\DataManager
+{
+	/**
+	 * Returns DB table name for entity.
+	 *
+	 * @return string
+	 */
+	public static function getTableName()
+	{
+		return 'b_tasks_member';
+	}
+
+	/**
+	 * Returns entity map definition.
+	 *
+	 * @return array
+	 */
+	public static function getMap()
+	{
+		return array(
+			'TASK_ID' => array(
+				'data_type' => 'integer',
+				'primary' => true,
+			),
+			'USER_ID' => array(
+				'data_type' => 'integer',
+				'primary' => true,
+			),
+			'TYPE' => array(
+				'data_type' => 'string',
+				'primary' => true,
+				'validation' => array(__CLASS__, 'validateType'),
+			),
+
+			// references
+			'USER' => array(
+				'data_type' => 'Bitrix\Main\User',
+				'reference' => array('=this.USER_ID' => 'ref.ID')
+			),
+			'TASK' => array(
+				'data_type' => 'Task',
+				'reference' => array('=this.TASK_ID' => 'ref.ID')
+			),
+			'TASK_FOLLOWED' => array(
+				'data_type' => 'Task',
+				'reference' => array(
+					'=this.TASK_ID' => 'ref.ID',
+					'=this.TYPE' => array('?', 'U')
+				)
+			),
+			'TASK_COWORKED' => array(
+				'data_type' => 'Task',
+				'reference' => array(
+					'=this.TASK_ID' => 'ref.ID',
+					'=this.TYPE' => array('?', 'A')
+				)
+			),
+		);
+	}
+	/**
+	 * Returns validators for TYPE field.
+	 *
+	 * @return array
+	 */
+	public static function validateType()
+	{
+		return array(
+			new Main\Entity\Validator\Length(null, 1),
+		);
+	}
+}
