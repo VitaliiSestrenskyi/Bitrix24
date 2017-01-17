6170b628669e6ce2ddd3841828b75fe10d407761
VitaliiSestrenskyi <sestrinskiy@mail.ru>
2017-01-17 17:41:02 +0200

    Update js.js
    ajax

--- js.js
+++ js.js
@@ -6,3 +6,34 @@ document.addEventListener("DOMContentLoaded", function (event)
         document.querySelector('.property-SERIYA_TSB').style.display = 'none';
     }
 });
+
+
+BX.showWait();
+var dataSend = new Object({
+    sessid:BX("sessid").value,
+    DEAL_ID:deal_id,
+    LOCATION_ID:document.querySelector("[name='UF_LOCATION']").value
+});
+var url = "/bitrix/admin/itua_novaposhta_getdealproducts.php";
+BX.ajax({
+    url: url,
+    data: dataSend,
+    method: 'POST',
+    dataType: 'json',
+    timeout: 30,
+    async: true,
+    processData: true,
+    scriptsRunFirst: true,
+    emulateOnload: true,
+    start: true,
+    cache: false,
+    onsuccess: function(data)
+    {
+        BX.closeWait();
+
+    },
+    onfailure: function()
+    {
+
+    }
+});
