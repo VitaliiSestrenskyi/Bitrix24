8e6cbdde9439b45c0d32ca710c2b9e5e38426d7f
VitaliiSestrenskyi <sestrinskiy@mail.ru>
2017-02-24 11:37:14 +0200

    AliasLoader.php
    AliasLoader.php

---
+++ AliasLoader.php
@@ -0,0 +1,33 @@
+class AliasLoader
+{
+    private $aliases = [];
+
+    private static $instance = null;
+    private function __construct(){}
+    protected function __clone(){}
+
+    public static function getInstance()
+    {
+        if(is_null(self::$instance))
+            self::$instance = new self();
+        return self::$instance;
+    }
+
+    private function getAliases()
+    {
+        return $this->aliases = [
+            'CItuaAfa' => '\Itua\Afa\CItuaAfa',
+            'CHelper' => '\Itua\Afa\CHelper',
+            'CItuaEvents' => '\Itua\Afa\CItuaEvents',
+            'CItuaAfaAgents' => '\Itua\Afa\CItuaAfaAgents',            
+        ];
+    }
+
+    public function register()
+    {
+        foreach( $this->getAliases() as  $alias=> $full )
+        {
+            class_alias ( $full , $alias , true );
+        }
+    }
+}
