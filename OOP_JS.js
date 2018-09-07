5ab2bb3654d4cf9be5149bf786c4aa92f992cc0c
VitaliiSestrenskyi <sestrinskiy@mail.ru>
2018-09-07 10:47:29 +0300

    ECMAScript 6
    ECMAScript 6

--- OOP_JS.js
+++ OOP_JS.js
@@ -41,3 +41,165 @@ if( typeof (CrmValidatorInput) === "undefined")
         return self;
     };
 }
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// ECMAScript 6 /////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+class CrmValidator
+{
+    constructor()
+    {
+        this.element = null;
+        this.validationTypes = {};
+        this.errorMessages = {};
+        this.result = [];
+    }
+
+    initialize(element, validationTypes, errorMessages)
+    {
+        this.setElement(element);
+        this.setValidationTypes(validationTypes);
+        this.setErrorMessages(errorMessages);
+    }
+
+    setResult(res)
+    {
+        this.result.push(res);
+    }
+    setElement(element)
+    {
+        this.element = element;
+    }
+    setValidationTypes(validationTypes)
+    {
+        this.validationTypes = validationTypes;
+    }
+    setErrorMessages(errorMessages)
+    {
+        this.errorMessages = errorMessages;
+    }
+    getResult()
+    {
+        return this.result;
+    }
+    getElement()
+    {
+        return this.element;
+    }
+    getValidationTypes()
+    {
+        return this.validationTypes;
+    }
+    getErrorMessages()
+    {
+        return this.errorMessages;
+    }
+
+
+    validate()
+    {
+    }
+    getFormattedResult()
+    {
+    }
+
+}
+
+
+
+class CrmValidatorInput extends CrmValidator
+{
+    validate()
+    {
+        let _validationTypes = this.getValidationTypes();
+        for(let i in _validationTypes)
+        {
+            switch (_validationTypes[i])
+            {
+                case 'isString':
+
+                    break;
+                case 'isNotEmptyString':
+                    if (typeof (this.getElementValue()) === "undefined" || this.getElementValue().length === 0)
+                    {
+                        this.setResult( this.getErrorMessages()[i] );
+                    }
+                    break;
+                case 'isBoolean':
+
+                    break;
+                case 'isNumber':
+                    if( isNaN( Number(this.getElementValue()) ) ||  Number(this.getElementValue()) === 0 )
+                    {
+                        this.setResult( this.getErrorMessages()[i] );
+                    }
+                    break;
+                case 'isMinusNumber':
+                    if (this.getElementValue().match(/^-\d+$/))
+                    {
+                        this.setResult( this.getErrorMessages()[i] );
+                    }
+                    break;
+                case 'isInteger':
+                    if(Number.isInteger( Number(this.getElementValue()) ) === false)
+                    {
+                        this.setResult( this.getErrorMessages()[i] );
+                    }
+                    break;
+                case 'isFunction':
+
+                    break;
+                case 'isDomNode':
+
+                    break;
+                case 'isElementNode':
+
+                    break;
+                case 'isArray':
+
+                    break;
+                case 'isDate':
+
+                    break;
+                default:
+
+                    break;
+            }
+        }
+    }
+
+    getElementValue()
+    {
+        return BX.util.trim( this.getElement().value );
+    }
+
+    getFormattedResult()
+    {
+        return this.getResult().join(", ");
+    }
+
+    static create(element, validationTypes, errorMessages)
+    {
+        let self = new CrmValidatorInput();
+        self.initialize(element, validationTypes, errorMessages);
+        return self;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
